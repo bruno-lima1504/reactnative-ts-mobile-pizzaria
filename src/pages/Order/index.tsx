773b8069800772f8fa23b0dbde5c7765815755c2
Bruno Lima <bruno.lima1504@gmail.com>
2024-04-02 16:56:25 -0300

    MVP commit

---
+++ src/pages/Order/index.tsx
@@ -0,0 +1,310 @@
+import React, { useState, useEffect } from "react";
+import { 
+    View, 
+    Text, 
+    StyleSheet,
+    TouchableOpacity, 
+    TextInput,
+    Modal,
+    FlatList
+} from "react-native";
+
+import { useRoute, RouteProp, useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { StackParamsList } from "../../routes/app.routes";
+
+import { Feather } from '@expo/vector-icons'
+
+import { api } from "../../services/api";
+
+import { ModalPicker } from "../../components/ModalPicker";
+import { ListItem } from "../../components/ListItem";
+
+type RouteDetailParams = {
+    Order:{
+        number: string | number;
+        order_id: string;
+    }
+}
+
+export type CategoryProps = {
+    id: string;
+    name: string;
+}
+
+type ProductProsps = {
+    id: string;
+    name: string;
+}
+
+type ItemProps = {
+    id: string;
+    product_id: string;
+    name: string;
+    amount: string | number;
+}
+
+type  OrderRouteProps = RouteProp<RouteDetailParams, 'Order'>
+
+export default function Order(){
+    const route = useRoute<OrderRouteProps>();
+    const navigation = useNavigation<NativeStackNavigationProp<StackParamsList>>();
+
+    const [category, setCategory] = useState<CategoryProps[] | []>([]);
+    const [categorySelected, setCategorySelected] = useState<CategoryProps | undefined>();
+    const [modalCategoryVisible, setModalCategoryVisible] = useState(false);
+    const [products, setProducts] = useState<ProductProsps[] | []>([]);
+    const [productSelected, setProductSelected] = useState<ProductProsps | undefined>();
+    const [modalProductVisible, setModalProductVisible] = useState(false);
+
+    const [amount, setAmount] = useState('1');
+
+    const [items, setItems] = useState<ItemProps[]>([]);    
+
+    useEffect(()=>{
+       async function loadInfo(){
+            const response = await api.get('/category');
+            setCategory(response.data);
+            setCategorySelected(response.data[0]);
+       } 
+
+       loadInfo()
+    }, [])
+
+    useEffect(()=>{
+        async function loadProducts(){            
+            const response = await api.get('/product/category',{
+                params:{
+                    category_id: categorySelected?.id
+                }
+            })               
+            setProducts(response.data);
+            setProductSelected(response.data[0])
+        };
+        loadProducts()
+    }, [categorySelected])
+
+    async function handleCloseOrder(){
+        try{
+            await api.delete('/order', {
+                params:{
+                    // passamos o params com "?" caso venha vazio evita dar erro e cai no cath
+                    order_id: route.params?.order_id
+                }
+            })            
+            navigation.goBack();
+        }catch(err){
+            console.log('erro -> '+ err)
+        }
+    }
+
+    function handleChangeCategory(item: CategoryProps){
+        setCategorySelected(item)
+    }
+
+    function handleChangeProduct(item: ProductProsps) {
+        setProductSelected(item)
+    }
+
+    //adiciona produto na mesa
+    async function handleAdd(){
+       const response = await api.post('order/add', {
+            order_id: route.params?.order_id,
+            product_id: productSelected?.id,
+            amount: Number(amount)
+       })
+
+       let data = {
+            id: response.data.id,
+            product_id: productSelected?.id as string,
+            name: productSelected?.name as string,
+            amount: amount
+       }
+       setItems(oldArray => [...oldArray, data])
+    }
+
+    async function handleDeleteItem(item_id: string){
+        await api.delete('/order/remove', {
+            params:{
+                item_id: item_id
+            }
+        })
+        //após remover do banco pela api temos que remover no fron
+        let removeItem = items.filter( item => {
+            return (item.id !== item_id)
+        })
+
+        setItems(removeItem);
+    }
+
+    function handleFinishOrder(){
+        navigation.navigate("FinishOrder", {number: route.params?.number, order_id: route.params?.order_id })
+    }
+    
+    return(
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.title}>Mesa {route.params.number}</Text>
+                {items.length === 0 && (
+                    <TouchableOpacity>
+                        <Feather
+                            name="trash-2"
+                            size={28}
+                            color="#FF3F4B"
+                            onPress={handleCloseOrder}
+                        />
+                    </TouchableOpacity>
+                )}
+            </View>            
+            {category.length !== 0 && (
+                <TouchableOpacity 
+                    style={styles.input}
+                    onPress={ () => setModalCategoryVisible(true) }
+                >
+                    <Text style={{ color: '#FFF' }}>
+                        {categorySelected?.name}
+                    </Text>
+                </TouchableOpacity>
+            )}
+
+            {products.length !== 0 && (
+                <TouchableOpacity 
+                style={styles.input}
+                onPress={ () => setModalProductVisible(true) }
+            >                
+                    <Text style={{ color: '#FFF' }}>
+                        {productSelected?.name}
+                    </Text>
+                </TouchableOpacity>
+            )}
+
+            <View style={styles.qtdContainer}>
+                <Text style={styles.qtdText}>
+                    Quantidade
+                </Text>
+                <TextInput
+                    style={[styles.input, { width: '60%', textAlign: 'center'}]}                     
+                    value={amount}
+                    keyboardType="numeric"
+                    onChangeText={setAmount}
+                />
+            </View>
+            <View style={styles.actions}>
+                <TouchableOpacity 
+                    style={styles.buttonAdd}
+                    onPress={handleAdd}
+                >
+                    <Text style={styles.buttonText}>+</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity 
+                    style={[styles.button, { opacity: items.length === 0 ? 0.3 : 1}]}
+                    disabled={items.length === 0}
+                    onPress={handleFinishOrder}
+                >
+                    <Text style={styles.buttonText}>Avançar</Text>
+                </TouchableOpacity>
+            </View>
+
+            <FlatList 
+                showsVerticalScrollIndicator={false}
+                style={{ flex: 1, marginTop: 24}}
+                data={items}
+                keyExtractor={(item) => item.id}
+                renderItem={ ({item }) =>  <ListItem data={item} deleteItem={handleDeleteItem} />}
+            />
+
+            <Modal
+                transparent={true}
+                visible={modalCategoryVisible}
+                animationType="fade"
+            >
+                <ModalPicker
+                    handleCloseModal={ () => { setModalCategoryVisible(false) }}
+                    options={category}
+                    selectedItem={ handleChangeCategory }
+                />
+            </Modal>
+            <Modal
+                transparent={true}
+                visible={modalProductVisible}
+                animationType="fade"
+            >
+                <ModalPicker
+                    handleCloseModal={() => { setModalProductVisible(false) }}
+                    options={products}
+                    selectedItem={handleChangeProduct}
+                />
+            </Modal>    
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#1D1D2E',
+        paddingVertical: '5%',
+        paddingEnd: '4%',
+        paddingStart: '4%'
+    },
+    header:{
+        flexDirection: 'row',
+        marginBottom: 12,
+        alignItems: 'center',
+        marginTop: 24
+    },
+    title:{
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#FFF',
+        marginRight: 14
+    },
+    input:{
+        backgroundColor: '#101026',
+        width: '100%',
+        borderRadius: 4,
+        height: 40,
+        marginBottom: 12,
+        color: '#FFF',
+        justifyContent: 'center',
+        paddingHorizontal: 8,
+        fontSize: 22,
+    },
+    qtdContainer:{
+        flexDirection: 'row',        
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    qtdText:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#FFF',
+    },
+    actions:{
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between'
+    },
+    buttonAdd:{
+        width: '20%',
+        backgroundColor: '#3fd1ff',
+        borderRadius: 4,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText:{
+        color: '#101026',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    button:{
+        backgroundColor: '#3fffa3',
+        borderRadius: 4,
+        height: 40,
+        width: '75%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,106 @@
+import React, {  useState, useContext } from "react";
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity, ActivityIndicator } from "react-native";
+
+import { AuthContext } from "../../contexts/AuthContext";
+
+export default function SignIn(){
+    const { signIn, loadingAuth } = useContext(AuthContext);
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function handleLogin(){
+        if (email === '' || password === ''){
+            return
+        }
+
+        await signIn({ email, password })
+    }
+
+    return(
+        <View style={styles.container}>
+            <Image 
+                style={styles.logo}
+                source={require('../../assets/logocortado.png')}
+            />  
+            <View style={styles.inputContainer}>
+                <TextInput 
+                    placeholder="Digite seu email"
+                    placeholderTextColor="#F0F0F0"
+                    style={styles.input}
+                    value={email}
+                    onChangeText={setEmail}
+                />
+                <TextInput 
+                    placeholder="Digite sua senha"
+                    placeholderTextColor="#F0F0F0"
+                    style={styles.input}
+                    secureTextEntry={true}
+                    value={password}
+                    onChangeText={setPassword}
+                />                
+                <TouchableOpacity 
+                    style={styles.button}
+                    onPress={handleLogin}
+                >
+                    {loadingAuth ? (
+                        <ActivityIndicator 
+                            size={25}
+                            color="#FFF" 
+                        />
+                    ) : (
+                        <Text 
+                            style={styles.buttonText}
+                        >
+                            Acessar
+                        </Text>
+
+                    )}
+                </TouchableOpacity>
+
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#1D1D2E',
+        color: "#F5F7FB" 
+    },
+    logo:{
+        marginBottom: 18
+    },
+    inputContainer: {
+        width: '95%',
+        alignContent: 'center',
+        justifyContent: 'center',
+        paddingVertical: 32,
+        paddingHorizontal: 14
+    },
+    input:{
+        width: '95%',
+        height: 40,
+        backgroundColor: '#101026',
+        marginBottom: 12,
+        borderRadius: 4,
+        paddingHorizontal: 8,
+        color: '#FFF' 
+    },
+    button:{
+        width: '95%',
+        height: 40,
+        backgroundColor: '#3FFFA3',
+        borderRadius: 4,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText:{
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: "#101026"
+    }
+})
